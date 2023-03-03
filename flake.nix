36cd31d7280eff6949b4e336b4b7fc5841609e56
Takeshi Abe <tabe@fixedpoint.jp>
2023-03-03 18:03:27 +0900

    Cover additional platforms

--- flake.nix
+++ flake.nix
@@ -1,38 +1,29 @@
 {
   description = "A flake for building thue";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.default =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        pname = "thue";
-        version = "0.0.1";
-        src = self;
-        buildPhase = "make";
-        doCheck = true;
-        checkPhase = "make check";
-        installPhase = ''
-          install -d $out/bin
-          install -m 0555 thue $out/bin
-        '';
+  outputs = { self, nixpkgs }:
+    let
+      derivation_thue = system:
+        with import nixpkgs { inherit system; };
+        stdenv.mkDerivation {
+          pname = "thue";
+          version = "0.0.1";
+          src = self;
+          buildPhase = "make";
+          doCheck = true;
+          checkPhase = "make check";
+          installPhase = ''
+            install -d $out/bin
+            install -m 0555 thue $out/bin
+          '';
       };
-
-    packages.aarch64-darwin.default =
-      with import nixpkgs { system = "aarch64-darwin"; };
-      stdenv.mkDerivation {
-        pname = "thue";
-        version = "0.0.1";
-        src = self;
-        buildPhase = "make";
-        doCheck = true;
-        checkPhase = "make check";
-        installPhase = ''
-          install -d $out/bin
-          install -m 0555 thue $out/bin
-        '';
-      };
-  };
+    in {
+      packages.aarch64-darwin.default = derivation_thue "aarch64-darwin";
+      packages.aarch64-linux.default  = derivation_thue "aarch64-linux";
+      packages.i686-linux.default     = derivation_thue "i686-linux";
+      packages.x86_64-darwin.default  = derivation_thue "x86_64-darwin";
+      packages.x86_64-linux.default   = derivation_thue "x86_64-linux";
+    };
 }
