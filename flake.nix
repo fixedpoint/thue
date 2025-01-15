7db09b4f16a161b710b98e98d33fd0b2a4e2c8f7
Takeshi Abe <tabe@fixedpoint.jp>
2025-01-15 11:18:06 +0900

    src layout
    This also upgrades nixpkgs and GitHub-hosted runners for CI.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   description = "A flake for building thue";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
-  outputs = { self, nixpkgs }:
-    let
-      derivation_thue = system:
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation {
-          pname = "thue";
-          version = "0.0.1";
-          src = self;
-          doCheck = true;
+  outputs = { self, nixpkgs }: let
+
+    allSystems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+    forAllSystems = nixpkgs.lib.genAttrs allSystems;
+
+    thue = system:
+      with nixpkgs.legacyPackages.${system};
+      stdenv.mkDerivation {
+        pname = "thue";
+        version = "0.0.1";
+        src = ./src;
+        doCheck = true;
       };
-    in {
-      packages.aarch64-darwin.default = derivation_thue "aarch64-darwin";
-      packages.aarch64-linux.default  = derivation_thue "aarch64-linux";
-      packages.i686-linux.default     = derivation_thue "i686-linux";
-      packages.x86_64-darwin.default  = derivation_thue "x86_64-darwin";
-      packages.x86_64-linux.default   = derivation_thue "x86_64-linux";
+
+    thue-packages = system: {
+      default = thue system;
     };
+
+  in {
+
+    packages = forAllSystems thue-packages;
+
+  };
 }
